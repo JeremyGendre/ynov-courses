3e941f70f9782215cf1f5e7de094602eb4d98293
Jérémy Gendre <jgendre@media-sante.com>
2020-10-13 15:07:10 +0200

    fix typo in homepage

--- react-course/src/page/HomePage/HomePage.tsx
+++ react-course/src/page/HomePage/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage(props:any){
             <div className="text-3xl">Projet regroupant les exercices des séances de React :)</div>
             <div className="w-full flex h-full">
                 <Link to="/exercices" className="m-auto text-blue-600 hover:underline">
-                    Voir les exercices ! <FontAwesomeIcon icon={faArrowRight}/>
+                    Voir les exercices <FontAwesomeIcon icon={faArrowRight}/>
                 </Link>
             </div>
         </div>
