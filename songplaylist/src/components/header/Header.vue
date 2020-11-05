2bd24c61ea33de5898b0dcf17972d9ac18aef413
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-05 15:35:25 +0100

    add HeaderLink component

--- songplaylist/src/components/header/Header.vue
+++ songplaylist/src/components/header/Header.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="header" class="fixed w-full d-flex justify-center py-4">
-        <div class="header-link relative mx-5 cursor-pointer px-2 py-1">Home</div>
-        <div class="header-link relative mx-5 cursor-pointer px-2 py-1">Playlist</div>
+        <HeaderLink text="Home" />
+        <HeaderLink text="Playlist" />
     </div>
 </template>
 
 <script>
     import '../../assets/css/Header.css';
+    import HeaderLink from "./HeaderLink";
     export default {
-        name: 'Header'
+        name: 'Header',
+        components: {HeaderLink}
     }
 </script>
---
+++ songplaylist/src/components/header/HeaderLink.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="header-link relative mx-5 cursor-pointer px-2 py-1">{{ text }}</div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderLink',
+        props: {
+            text: String
+        }
+    };
+</script>
