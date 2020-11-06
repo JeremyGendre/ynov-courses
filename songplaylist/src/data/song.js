759ee805ad089ce415a0279598187608445daec2
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-06 12:36:16 +0100

    remove test title

--- songplaylist/src/data/song.js
+++ songplaylist/src/data/song.js
@@ -12,6 +12,6 @@ export class Song{
 export const songList = [
     new Song('/songs/Alan Walker - Faded.mp3', "Faded",'Alan Walker'),
     new Song('/songs/Alan Walker - The Spectre.mp3', "The Spectre",'Alan Walker'),
-    new Song('/songs/roby-fayer-ready-to-fight-fttom-gefen.mp3', 'Ready to fightfightfightfightfightfightfightfightfightfight', 'Roby Fayer'),
+    new Song('/songs/roby-fayer-ready-to-fight-fttom-gefen.mp3', 'Ready to fight', 'Roby Fayer'),
     new Song('/songs/wolves-my-time.mp3', "My time", "Wolves"),
 ];
