c3ae06b892fde33b61da517ac6a66eb937445525
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-08 13:40:57 +0100

    remove songplaylist directory to put it in a proper repository

--- songplaylist/src/plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import {mainTheme} from "../helpers/theme";
-
-Vue.use(Vuetify);
-
-export default new Vuetify({
-    theme: mainTheme
-});
