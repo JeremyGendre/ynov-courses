9209eb44c62e8298ec159569b95411daf3f11a98
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-09 11:09:06 +0200

    feat: click event & fade out

--- angular-1/src/app/app.component.ts
+++ angular-1/src/app/app.component.ts
@@ -9,19 +9,23 @@ export class AppComponent {
   title = 'angular-1';
   boxes = [
     {
-      title: 'Headline 1',
+      title: 'Headline 1', id: 1,
       content: 'lorem ipsum lorem ispum 1',
       color: '#F04040',
     },
     {
-      title: 'Headline 2',
+      title: 'Headline 2', id: 2,
       content: 'lorem ipsum lorem ispum 2',
       color: '#8A6F6F',
     },
     {
-      title: 'Headline 3',
+      title: 'Headline 3', id: 3,
       content: 'lorem ipsum lorem ispum 3',
       color: '#C2F040',
     },
-  ]
+  ];
+
+  handleClickBox(boxId: number): void{
+    this.boxes = this.boxes.filter(box => box.id !== boxId);
+  }
 }
--- angular-1/src/app/box/box.component.ts
+++ angular-1/src/app/box/box.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-box',
@@ -6,13 +6,24 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./box.component.scss']
 })
 export class BoxComponent implements OnInit {
+  @Input() id: number = 0;
   @Input() title: string = 'Headline';
   @Input() content: string = 'lorem ipsum lorem ipsum';
   @Input() color: string = "red";
+  clicked: boolean = false;
+
+  @Output() clickedEvent = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleClick() : void{
+    this.clicked = true;
+    setTimeout(() => {
+      this.clickedEvent.emit(this.id);
+    },1000);
   }
 
 }
