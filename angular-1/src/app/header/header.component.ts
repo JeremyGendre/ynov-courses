0c70d21b0e3f7421489f1d7fe34b17c653102e9b
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-10 11:47:32 +0200

    feat: remove resize event on destroy

--- angular-1/src/app/header/header.component.ts
+++ angular-1/src/app/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   routes = [
     {link: '/app', title: 'Home'},
@@ -20,11 +20,17 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.addEventListener('resize', () => {
-      if(window.innerWidth > 650){
-        this.menuOpened = false;
-      }
-    });
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.onResize.bind(this));
+  }
+
+  onResize(){
+    if(window.innerWidth > 650 && this.menuOpened){
+      this.menuOpened = false;
+    }
   }
 
   handleClick(){
